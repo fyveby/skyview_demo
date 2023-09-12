f7993725458a31c76eeba9200c349ac6e57f7d1e
Oscar Zhang <oscarzhang228@gmail.com>
2023-09-12 10:24:35 -0400

    embed google form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import { Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Demo from "./Components/Demo";
 import DemoProfile from "./Components/DemoProfile";
+import DemoContact from "./Components/DemoContact";
 
 function App() {
   return (
     <Router>
-      <Route path="/demo" element={<Demo></Demo>}></Route>
-      <Route path="/demo/profile" element={<DemoProfile></DemoProfile>}></Route>
+      <Routes>
+        <Route path="/demo" element={<Demo></Demo>}></Route>
+        <Route
+          path="/demo/profile"
+          element={<DemoProfile></DemoProfile>}
+        ></Route>
+        <Route
+          path="/demo/contact"
+          element={<DemoContact></DemoContact>}
+        ></Route>
+      </Routes>
     </Router>
   );
 }
