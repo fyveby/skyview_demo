440fc9625df49cb659b9ef5e35019a9994ea2f83
oscarzhangFyve <144487943+oscarzhangFyve@users.noreply.github.com>
2023-09-12 11:29:01 -0400

    test

--- src/App.js
+++ src/App.js
@@ -2,23 +2,17 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Demo from "./Components/Demo";
-import DemoProfile from "./Components/DemoProfile";
-import DemoContact from "./Components/DemoContact";
+import Demo from "./Demo";
+import DemoProfile from "./DemoProfile";
+import DemoContact from "./DemoContact";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/demo" element={<Demo></Demo>}></Route>
-        <Route
-          path="/demo/profile"
-          element={<DemoProfile></DemoProfile>}
-        ></Route>
-        <Route
-          path="/demo/contact"
-          element={<DemoContact></DemoContact>}
-        ></Route>
+        <Route path="/" element={<Demo></Demo>}></Route>
+        <Route path="/profile" element={<DemoProfile></DemoProfile>}></Route>
+        <Route path="/contact" element={<DemoContact></DemoContact>}></Route>
       </Routes>
     </Router>
   );
