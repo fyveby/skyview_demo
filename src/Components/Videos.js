2e05bed6aa386ab6254932980b805e76f33d2005
oscarzhangFyve <144487943+oscarzhangFyve@users.noreply.github.com>
2023-09-14 16:35:42 -0400

    remove controls for videos, autoplay, and loop recordings

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -3,7 +3,7 @@ function Videos() {
     <div className="p-5 bg-body-tertiary rounded-3">
       <div className="row">
         <div className="col-md-6 d-flex justify-content-center">
-          <video controls height={250} width={250}>
+          <video autoPlay muted loop height={250} width={250}>
             <source
               src="https://nbaa-demo-video-storage.s3.amazonaws.com/mixkit-airplane-taking-off-in-the-sun-27988-medium.mp4"
               type="video/mp4"
@@ -11,7 +11,7 @@ function Videos() {
           </video>
         </div>
         <div className="col-md-6 d-flex justify-content-center">
-          <video controls height={250} width={250}>
+          <video autoPlay muted loop height={250} width={250}>
             <source
               src="https://nbaa-demo-video-storage.s3.amazonaws.com/mixkit-airplane-taking-off-in-the-sun-27988-medium.mp4"
               type="video/mp4"
@@ -21,7 +21,7 @@ function Videos() {
       </div>
       <div className="row">
         <div className="col-md-6 d-flex justify-content-center">
-          <video controls height={250} width={250}>
+          <video autoPlay muted loop height={250} width={250}>
             <source
               src="https://nbaa-demo-video-storage.s3.amazonaws.com/mixkit-airplane-taking-off-in-the-sun-27988-medium.mp4"
               type="video/mp4"
@@ -29,7 +29,7 @@ function Videos() {
           </video>
         </div>
         <div className="col-md-6 d-flex justify-content-center">
-          <video controls height={250} width={250}>
+          <video autoPlay muted loop height={250} width={250}>
             <source
               src="https://nbaa-demo-video-storage.s3.amazonaws.com/mixkit-airplane-taking-off-in-the-sun-27988-medium.mp4"
               type="video/mp4"
